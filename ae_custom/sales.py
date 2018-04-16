41799ab9d58de7c3eb30bc414b6e70ad0b5630b4
Atul <jain.atul43@gmail.com>
2018-04-16 18:09:25 +0530

    label sent code is done

--- ae_custom/sales.py
+++ ae_custom/sales.py
@@ -14,15 +14,26 @@ class sale_order_line(osv.osv):
         result = {}
         for id in ids:
             sales = self.pool.get('sale.order.line')
+            # import pdb; pdb.set_trace()
             sales_id = sales.search(cr, uid,  [('id', '=', id)])
             if sales_id:
                 sales_ob = sales.browse(cr, uid, sales_id[0], context=context)
-                if sales_ob.order_id.shipped == True:
-                    result[id] = True
-                    print "----false"
-                else:
+                stock_pi = self.pool.get('stock.picking.out')
+                stock_pi_id = stock_pi.search(cr, uid, [('sale_id', '=', sales_ob.order_id.id)])
+                if len(stock_pi_id) == 0:
                     result[id] = False
-                    print "-----true"
+                else:
+                    stock_picking_obj = stock_pi.browse(cr, uid, stock_pi_id[0], context=context)
+                    stock = self.pool.get('stock.move')
+                    stk_id = stock.search(cr, uid,  [('sale_line_id', '=', id)])
+                    stk_obj = stock.browse(cr, uid, stk_id[0], context=context)
+                    if stock_picking_obj.state == 'done':
+                        if stk_obj.state == 'done':
+                            result[id] = True
+                            print "----false"
+                    else:
+                        result[id] = False
+                        print "-----true"
             else:
                 result[id] = False
                 print "----true"
@@ -45,3 +56,13 @@ sale_order_line()
 # state
 # origin
 # sale_line_id
+# cr.execute('SELECT origin FROM stock_picking WHERE stock_picking.id = %s', tmp)
+#         origin = cr.fetchone()
+#         origin = origin[0] 
+#         origin = int(origin[2:])
+#         cr.execute('SELECT id FROM sale_order_line WHERE order_id = %s AND product_id=%s', (origin, vals['product_id']))                
+#         zsli = cr.fetchone()    
+#         zsli = zsli[0]    
+#         vals['x_sale_line_id'] = zsli 
+#         res_id = super(stock_pack_operation, self).create(cr, uid, vals, context=context)
+#         return res_id
